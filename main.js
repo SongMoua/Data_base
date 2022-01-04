dbab2a4ff4f666a0ec5985f36c3837bf473bd482
SongMoua <86039593+SongMoua@users.noreply.github.com>
2022-01-04 11:11:33 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+
+let btn = document.getElementById('btn');
+let output = document.getElementById('display_length');
+
+btn.addEventListener('click', function(){
+
+    let str = document.getElementById('user_word_input').value;
+
+    display_length.innerHTML = str.length;
+})
+
+
+
+document.getElementById("clickMe").click();
+
+
+function automate_clicking(){
+    let str = document.getElementById('user_word_input').value;
+
+    display_length.innerHTML = str.length;
+}
+
+
+ 
+
+
+ 
+
+ 
